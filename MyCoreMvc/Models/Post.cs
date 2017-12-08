e60f878e73bdad57eba8d1fe5e8a107e6da7c77f
Marcel Wijnen <wijnenmarcel@hotmail.com>
2017-12-08 22:56:56 +0100

    Added post and comment model class

---
+++ MyCoreMvc/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreMvc.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string UpdatedBy { get; set; }
+        public string CategoryId { get; set; }
+        public string Abstract { get; set; }
+        public string Contents { get; set; }
+    }
+}
