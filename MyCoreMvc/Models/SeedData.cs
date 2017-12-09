f7eedf766a8b7414c964ed34cd73c27bd8082244
Marcel Wijnen <mmfwwijnen@gmail.com>
2017-12-09 16:41:53 +0100

    Added seed class for Products

---
+++ MyCoreMvc/Models/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyCoreMvc.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product { Name = "Kayak", Description = "A boat for one person", Category = "Watersports", Price = 275 },
+                    new Product { Name = "Soccer Ball", Description = "FIFA-approved size and weight", Category = "Soccer", Price = 19.50m }
+                    );
+            }
+        }
+    }
+}
