091dba1bd5a3f80198aa617ecbfddf4819291ca8
Marcel Wijnen <mmfwwijnen@gmail.com>
2017-12-17 12:06:55 +0100

    Cleaned some files and adjusted the SeedProduct class

---
+++ MyCoreMvc/Models/Product/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreMvc.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Product> Products => context.Products;
+
+    }
+}
